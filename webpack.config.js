5a57620dc398b97af09374844a028942516bd20c
Glenn Dwiyatcita <dwiyatci@mail.com>
2018-01-29 00:18:47 +0100

    :tada: feat(webpack 4): initial WIP

--- webpack.config.js
+++ webpack.config.js
@@ -1,121 +1,83 @@
 /**
- * Created by glenn on 30.04.17.
- * Last updated on 30.12.17.
+ * Created by glenn on 05.08.16.
+ * Last updated on 29.01.18.
  */
 
 const { resolve } = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
 
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
-
-const config = {
-  entry: {
-    app: './src/app.js',
-    vendor: [
-      'lodash',
-      'jquery',
-    ],
-    polyfills: [
-      'babel-polyfill',
-    ],
-  },
-  output: {
-    filename: ifProd('[name].[chunkhash].js', '[name].js'),
-    path: resolve(__dirname, 'assets'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          resolve(__dirname, 'src'),
-        ],
-        use: {
-          // https://webpack.js.org/loaders/babel-loader
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
+module.exports = (env) => {
+  const config = {
+    mode: eitherProdOrDev('production', 'development'),
+    // output: {
+    //   filename: eitherProdOrDev('[name].[chunkhash].js', '[name].js'),
+    // },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [
+            resolve(__dirname, 'src'),
+          ],
+          use: {
+            // https://webpack.js.org/loaders/babel-loader
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({ use: 'css-loader' }),
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader',
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: 'url-loader?limit=8192',
-      },
-      {
-        test: /\.json$/,
-        use: 'json-loader',
-      },
-    ],
-  },
-  plugins: [
-    // Code Splitting - CSS
-    new ExtractTextPlugin(ifProd('[name].[chunkhash].css', '[name].css')),
-
-    // Code Splitting - Libraries
-    new webpack.optimize.CommonsChunkPlugin({
-      // https://stackoverflow.com/q/39548175/2013891
-      // The order of this array matters.
-      names: [
-        'polyfills',
-        'vendor',
-        'manifest',
+        {
+          test: /\.css$/,
+          // use: ExtractTextPlugin.extract({ use: 'css-loader' }),
+          use: 'css-loader',
+        },
+        {
+          test: /\.html$/,
+          use: 'html-loader',
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: 'url-loader?limit=8192',
+        },
+        {
+          test: /\.json$/,
+          type: 'json',
+        },
       ],
-    }),
+    },
+    plugins: [
+      // Code Splitting - CSS
+      // new ExtractTextPlugin(eitherProdOrDev('[name].[chunkhash].css', '[name].css')),
 
-    // Caching
-    new HtmlWebpackPlugin({
-      template: './src/index.ejs',
-      favicon: './src/favicon.ico',
-    }),
-    new InlineManifestWebpackPlugin(),
-
-    ...ifProd(
-      [
-        // Building for Production
-        // new webpack.LoaderOptionsPlugin({ minimize: true }), - not needed anymore?
-        new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-      ],
-      [
-        new webpack.NoEmitOnErrorsPlugin(),
-
-        // HMR
-        new webpack.HotModuleReplacementPlugin(),
-      ]
-    ),
-
-    // Scope hoisting
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
-  devtool: ifProd('source-map', 'eval'),
-  resolve: {
-    modules: [
-      'node_modules',
-      resolve(__dirname, 'src'),
+      // Caching
+      // new HtmlWebpackPlugin({
+      //   template: './src/index.ejs',
+      //   favicon: './src/favicon.ico',
+      // }),
+      // new InlineManifestWebpackPlugin(),
     ],
-  },
-  devServer: {
-    contentBase: resolve(__dirname, 'assets'),
-    compress: true,
-    noInfo: true,
-    historyApiFallback: true,
-    hot: true,
-    https: true,
-  },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      runtimeChunk: true,
+    },
+    devServer: {
+      contentBase: resolve(__dirname, 'dist'),
+      compress: true,
+      noInfo: true,
+      historyApiFallback: true,
+      hot: true,
+      https: true,
+    },
+  };
+
+  return config;
+
+  function eitherProdOrDev(prodStuff, devStuff) {
+    return (env && env.production) ? prodStuff : devStuff;
+  }
 };
-
-function ifProd(prodStuff, devStuff) {
-  return (process.env.NODE_ENV === 'production') ? prodStuff : devStuff;
-}
-
-module.exports = config;
